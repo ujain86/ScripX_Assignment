29fdb7a2041bd239be132686d67afc75105270fc
Ujjwal <ujjwal.jain116@gmail.com>
2023-05-27 17:55:40 +0530

    Updated readme file

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div id="navbar">Avengers</div>
+    <div id="navbar">
+      <h1 style={{color: 'white', padding: 30, marginLeft: 10}}>ScripX</h1>
+    </div>
   )
 }
 
